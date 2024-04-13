a5cd9ca6725c4f5c3ea1c484701187fffdbaeea0
sanvirk99 <sanseerat99@gmail.com>
2024-04-13 11:14:52 -0700

    render test

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const http=require('http')
 const express=require('express');
 const { time } = require('console');
-const morgan = require('morgan')
-
+const morgan = require('morgan');
+const cors= require('cors');
 
 const app=express();
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
-app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.json());
+app.use(cors());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 
 let persons=
