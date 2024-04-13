2d67f03ec8f09f97de88d2bc83e60035273fd53c
sanvirk99 <sanseerat99@gmail.com>
2024-04-13 11:35:18 -0700

    add front end

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app=express();
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
+app.use(express.static('dist'))
 app.use(express.json());
 app.use(cors());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
